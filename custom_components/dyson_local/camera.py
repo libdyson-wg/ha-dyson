7e038c1206246dc30e2eb53476d1193ea8dcc02c
Ben Vezzani <dotvezz@gmail.com>
2023-07-12 15:16:03 -0400

    Moving camera.py to dyson_local so it can be configured (even though it's technically a cloud feature)

--- custom_components/dyson_local/camera.py
+++ custom_components/dyson_local/camera.py
@@ -4,12 +4,12 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components.camera import Camera
-from ..vendor.libdyson.const import DEVICE_TYPE_360_EYE, DEVICE_TYPE_360_HEURIST
-from ..vendor.libdyson.cloud.cloud_360_eye import DysonCloud360Eye
-from ..vendor.libdyson.cloud import DysonDeviceInfo
+from custom_components.dyson_local.vendor.libdyson.const import DEVICE_TYPE_360_EYE, DEVICE_TYPE_360_HEURIST
+from custom_components.dyson_local.vendor.libdyson.cloud.cloud_360_eye import DysonCloud360Eye
+from custom_components.dyson_local.vendor.libdyson.cloud import DysonDeviceInfo
 from datetime import timedelta
 
-from .const import DATA_ACCOUNT, DATA_DEVICES, DOMAIN
+from custom_components.dyson_local.cloud.const import DATA_ACCOUNT, DATA_DEVICES, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
