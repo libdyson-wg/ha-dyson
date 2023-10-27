f3d3331a5a73db9c726f95154a92cd6786272ee7
Ben Vezzani <dotvezz@gmail.com>
2023-10-26 22:46:55 -0400

    Removes old dead tests, adds initial support for filter life resets

---
+++ custom_components/dyson_local/button.py
@@ -0,0 +1,51 @@
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_NAME
+from homeassistant.components.button import ButtonEntity, ButtonDeviceClass
+
+from typing import Callable, Optional
+
+from .const import DATA_COORDINATORS, DATA_DEVICES, DOMAIN
+
+from . import DysonEntity, DysonDevice
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities: Callable
+) -> None:
+    """Set up Dyson button from a config entry."""
+    device = hass.data[DOMAIN][DATA_DEVICES][config_entry.entry_id]
+    name = config_entry.data[CONF_NAME]
+
+    _LOGGER.error(f"one {name}")
+
+    entities = []
+
+    if hasattr(device, "filter_life"):
+        _LOGGER.error(f"two {name}")
+        entities.append(DysonFilterResetButton(device, name))
+
+    async_add_entities(entities)
+
+
+class DysonFilterResetButton(DysonEntity, ButtonEntity):
+    _attr_entity_category = EntityCategory.CONFIG
+
+    @property
+    def sub_name(self) -> Optional[str]:
+        """Return the name of the Dyson button."""
+        return "Reset Filter Life"
+
+    @property
+    def sub_unique_id(self) -> str:
+        """Return the button's unique id."""
+        return "reset-filter"
+
+    def press(self) -> None:
+        self._device.reset_filter()
